79a53c9ceab63ee6525665a2863f1776edbabd39
josepernalete2 <jose00pg2@gmail.com>
2026-08-02 09:51:01 -0400

    Definición y asignación de los pines

---
+++ semaforo.cpp
@@ -0,0 +1,26 @@
+// Semáforo 1
+const int PIN_S1_V_VEH = 13; // Verde Vehículos Semáforo 1
+const int PIN_S1_A_VEH = 12; // Amarillo Vehículos Semáforo 1
+const int PIN_S1_R_VEH = 11; // Rojo Vehículos Semáforo 1
+const int PIN_S1_R_PEA = 10; // Rojo Peatonal Semáforo 1
+const int PIN_S1_V_PEA = 9;  // Verde Peatonal Semáforo 1
+
+// Semáforo 2
+const int PIN_S2_V_VEH = 8;  // Verde Vehículos Semáforo 2
+const int PIN_S2_A_VEH = 7;  // Amarillo Vehículos Semáforo 2
+const int PIN_S2_R_VEH = 6;  // Rojo Vehículos Semáforo 2
+const int PIN_S2_R_PEA = 5;  // Rojo Peatonal Semáforo 2
+const int PIN_S2_V_PEA = 4;  // Verde Peatonal Semáforo 2
+
+// Semáforo 3
+const int PIN_S3_V_VEH = 3;  // Verde Vehículos Semáforo 3
+const int PIN_S3_A_VEH = 2;  // Amarillo Vehículos Semáforo 3
+const int PIN_S3_R_VEH = 1;  // Rojo Vehículos Semáforo 3
+const int PIN_S3_R_PEA = 0;  // Rojo Peatonal Semáforo 3
+
+// Semáforo 4 (Pines analógicos como digitales)
+const int PIN_S4_V_VEH = A0; // Verde Vehículos Semáforo 4
+const int PIN_S4_A_VEH = A1; // Amarillo Vehículos Semáforo 4
+const int PIN_S4_R_VEH = A2; // Rojo Vehículos Semáforo 4
+const int PIN_S4_R_PEA = A3; // Rojo Peatonal Semáforo 4
+const int PIN_S4_V_PEA = A4; // Verde Peatonal Semáforo 4
