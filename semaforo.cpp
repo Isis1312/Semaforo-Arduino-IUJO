c899a1f118bde51dc42b4e49cd0a8ffc720b1cf8
Isis Cedeño <isis01sofic@gmail.com>
2026-08-02 09:55:28 -0400

    Configuración de tiempos y setup

--- semaforo.cpp
+++ semaforo.cpp
@@ -24,3 +24,40 @@ const int PIN_S4_A_VEH = A1; // Amarillo Vehículos Semáforo 4
 const int PIN_S4_R_VEH = A2; // Rojo Vehículos Semáforo 4
 const int PIN_S4_R_PEA = A3; // Rojo Peatonal Semáforo 4
 const int PIN_S4_V_PEA = A4; // Verde Peatonal Semáforo 4
+
+//Configruación de tiempos
+
+const int TIEMPO_VERDE_VEH = 5000;    // 5 segundos
+const int TIEMPO_AMARILLO_VEH = 2000; // 2 segundos
+const int TIEMPO_TODO_ROJO = 1000;    // 1 segundo
+const int TIEMPO_PEA_PARPADEO = 250;  // Parpadeo peatonal
+
+void setup() {
+  // Configuración de pines S1
+  pinMode(PIN_S1_V_VEH, OUTPUT);
+  pinMode(PIN_S1_A_VEH, OUTPUT);
+  pinMode(PIN_S1_R_VEH, OUTPUT);
+  pinMode(PIN_S1_R_PEA, OUTPUT);
+  pinMode(PIN_S1_V_PEA, OUTPUT);
+  
+  // Configuración de pines S2
+  pinMode(PIN_S2_V_VEH, OUTPUT);
+  pinMode(PIN_S2_A_VEH, OUTPUT);
+  pinMode(PIN_S2_R_VEH, OUTPUT);
+  pinMode(PIN_S2_R_PEA, OUTPUT);
+  pinMode(PIN_S2_V_PEA, OUTPUT);
+  
+  // Configuración de pines S3
+  pinMode(PIN_S3_V_VEH, OUTPUT);
+  pinMode(PIN_S3_A_VEH, OUTPUT);
+  pinMode(PIN_S3_R_VEH, OUTPUT);
+  pinMode(PIN_S3_R_PEA, OUTPUT);
+
+  // Configuración de pines S4
+  pinMode(PIN_S4_V_VEH, OUTPUT);
+  pinMode(PIN_S4_A_VEH, OUTPUT);
+  pinMode(PIN_S4_R_VEH, OUTPUT);
+  pinMode(PIN_S4_R_PEA, OUTPUT);
+  pinMode(PIN_S4_V_PEA, OUTPUT);
+}
+
